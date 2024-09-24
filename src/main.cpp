648825ebc6e5999d6c70ec2e895e6d4acd751e0d
Wassim Akkari <was.akkari@gmail.com>
2024-09-24 21:07:36 +0100

    Add static cast and showing how it can lead to UB (Undefined Behaviour)

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,38 @@
 #include <iostream>
-int main(int, char**){
 
+class Parent
+{
+public:
+    void speak()
+    {
+        std::cout << "parent!" << std::endl;
+    }
+};
+
+class Brother : public Parent
+{
+public:
+    void speak()
+    {
+        std::cout << "brother!" << std::endl;
+    }
+};
+
+class Sister : public Parent
+{
+};
+
+int main(int argc, char *argv[])
+{
+    Parent parent;
+    Brother brother;
+
+    Parent *ppb = &parent;
+
+    Brother *pbb = static_cast<Brother *>(ppb);
+
+    std::cout << pbb << std::endl;
+    pbb->speak(); // Undefined Behaviour - pointed object is parent and not brother
+
+    return 0;
 }
