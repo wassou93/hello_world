c54991800a4d7c0c3d4a0808c70378fc7d4f2652
Wassim Akkari <was.akkari@gmail.com>
2024-09-24 19:08:08 +0100

    multiply two matrix

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <array>
 
-int main(int, char**){
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+static const int N = 2;
+
+array<array<int, N>, N> multiply_matrix(
+    const array<array<int, N>, N>& mat1, 
+    const array<array<int, N>, N>& mat2
+    ) 
+{
+    array<array<int, N>, N> res = {{{0, 0}, {0, 0}}};
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            for (int k = 0; k < N; ++k) {
+                res[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+    return res;
+}
+
+int main(int, char**) {
+    array<array<int, N>, N> m1 = {{{1, 2}, {3, 4}}};
+    array<array<int, N>, N> m2 = {{{2, 3}, {4, 5}}};
+    auto res = multiply_matrix(m1, m2);
+
+    for (const auto& row : res) {
+        for (const auto& elem : row) {
+            cout << elem << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
