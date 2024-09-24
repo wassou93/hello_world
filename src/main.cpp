2c05ea32e404aaa710f934cb2e6b331fe376bff1
Wassim Akkari <was.akkari@gmail.com>
2024-09-24 20:03:52 +0100

    preparing for next problem

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,4 @@
 #include <iostream>
-
-int getInt() {
-    return 1000;
-}
-
 int main(int, char**){
-    int value1 = 700;
-    int *pValue2 = &value1;
-    // int *pValue3 = &700;
 
-    int *pValue4 = &++value1;
-    // int *pValue5 = &value1++;
-
-    // int *pValue5 = &getInt();
 }
