2436722d41cf72f3d40331c8181bb83a1eb2ca62
Wassim Akkari <was.akkari@gmail.com>
2024-09-24 19:58:38 +0100

    Add high resolution clock to measure execution time.

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,19 @@
 
 using namespace std;
 
-static const int N = 2;
+static const int N = 100;
 
 array<array<int, N>, N> multiply_matrix(
-    const array<array<int, N>, N>& mat1, 
-    const array<array<int, N>, N>& mat2
-    ) 
+    const array<array<int, N>, N> &mat1,
+    const array<array<int, N>, N> &mat2)
 {
-    array<array<int, N>, N> res = {{{0, 0}, {0, 0}}};
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            for (int k = 0; k < N; ++k) {
+    array<array<int, N>, N> res = {2};
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            for (int k = 0; k < N; ++k)
+            {
                 res[i][j] += mat1[i][k] * mat2[k][j];
             }
         }
@@ -21,16 +23,29 @@ array<array<int, N>, N> multiply_matrix(
     return res;
 }
 
-int main(int, char**) {
-    array<array<int, N>, N> m1 = {{{1, 2}, {3, 4}}};
-    array<array<int, N>, N> m2 = {{{2, 3}, {4, 5}}};
-    auto res = multiply_matrix(m1, m2);
+#include <chrono>
 
-    for (const auto& row : res) {
-        for (const auto& elem : row) {
+int main(int, char **)
+{
+    array<array<int, N>, N> m1 = {2};
+    array<array<int, N>, N> m2 = {2};
+
+    auto start = chrono::high_resolution_clock::now();
+    auto res = multiply_matrix(m1, m2);
+    auto end = chrono::high_resolution_clock::now();
+
+    chrono::duration<float, micro> elapsed = end - start;
+
+    for (const auto &row : res)
+    {
+        for (const auto &elem : row)
+        {
             cout << elem << " ";
         }
         cout << endl;
     }
+
+    cout << "Time taken: " << elapsed.count() << " microseconds" << endl;
+
     return 0;
 }
